360003e8f811cfb90aee8d49705c0f634c0c8d54
Suraj Rana <surajrana.0899@gmail.com>
2026-04-02 23:06:43 +0545

    null safety principle

--- lib/multiplication.dart
+++ lib/multiplication.dart
@@ -4,10 +4,10 @@ void main(){
   print('Enter positive integer you want a tanle.');
   int numb = int.parse(stdin.readLineSync()!);
 
-  if (numb == null || numb <= 0) {
-    print('Please enter a valid positive integer.');
-    return;
-  }
+  // if (numb == null || numb <= 0) {
+  //   print('Please enter a valid positive integer.');
+  //   return;
+  // }
 
   for (int i = 1; i <= 10; i++) {
     int result = numb * i;
