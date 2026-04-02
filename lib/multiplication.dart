aa762682b75803632ede7e8a90724cf1ec6a9c93
Suraj Rana <surajrana.0899@gmail.com>
2026-04-02 22:15:30 +0545

    Last question

---
+++ lib/multiplication.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main(){
+  print('Enter positive integer you want a tanle.');
+  int numb = int.parse(stdin.readLineSync()!);
+
+  if (numb == null || numb <= 0) {
+    print('Please enter a valid positive integer.');
+    return;
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    int result = numb * i;
+    print('$numb x $i = $result');
+  }
+}
